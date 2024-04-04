1bba5a94af5b85f15e28b225803f7acacf3a3166
Saurav Upadhyay <116784047+upsaurav12@users.noreply.github.com>
2024-04-04 21:04:17 +0530

    Revert "fix: added missing gems"

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -57,14 +57,6 @@ class LocationsController < ApplicationController
     end
   end
 
-  def search
-    if params[:city].present?
-      @places = Place.where(city: params[:city])
-    else
-      flash[:error] = "Please provide a city name"
-      @places = []
-    end
-  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_location
